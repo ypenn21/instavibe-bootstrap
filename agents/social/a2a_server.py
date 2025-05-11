f5214c577113578894699733b63ec341b45b8c14
Christina Lin <weimeilin@gmail.com>
2025-05-11 16:29:11 -0400

    Update a2a_server.py

---
+++ agents/social/a2a_server.py
@@ -0,0 +1,17 @@
+from common.server import A2AServer
+from common.types import AgentCard, AgentCapabilities, AgentSkill
+from common.task_manager import AgentTaskManager
+from social.social_agent import SocialAgent
+
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+host=os.environ.get("A2A_HOST", "localhost")
+port=int(os.environ.get("A2A_PORT",10001))
+PUBLIC_URL=os.environ.get("PUBLIC_URL")
