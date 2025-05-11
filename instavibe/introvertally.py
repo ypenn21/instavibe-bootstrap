40478bd6584116176b3bc5a79655511fdc18f5ae
Christina Lin <weimeilin@gmail.com>
2025-05-11 09:27:23 -0400

    Update introvertally.py

--- instavibe/introvertally.py
+++ instavibe/introvertally.py
@@ -6,11 +6,9 @@ import os
 
 load_dotenv()
 
-ORCHESTRATE_AGENT_ID = os.environ.get('ORCHESTRATE_AGENT_ID',"projects/789872749985/locations/us-central1/reasoningEngines/7321524784058073088")
-
-agent_engine = agent_engines.get(ORCHESTRATE_AGENT_ID)
-
+#REPLACE ME initiate agent_engine
 
 #REPLACE ME call_agent_for_plan
+
 #REPLACE ME post_plan_event
 
