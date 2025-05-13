4ac3f1b7eb2e949136b1bba50d531bfc7e9c420f
Christina Lin <weimeilin@gmail.com>
2025-05-13 09:34:39 -0400

    Update remote_delete.py

--- runners/remote_delete.py
+++ runners/remote_delete.py
@@ -1,23 +1,9 @@
 from vertexai import agent_engines
+import os
 
-#print("Listing Agent Engines:")
-#agent_list = agent_engines.list()
-
-
-
-agent_list = agent_engines.list()
-print("Available Agent Engines:")
-if agent_list:
-    for agent in agent_list:
-        # Assuming agent objects have 'display_name' and 'resource_name' attributes
-        print(f"  Display Name: {getattr(agent, 'display_name', 'N/A')}, Resource Name: {getattr(agent, 'resource_name', 'N/A')}")
-else:
-    print("  No agent engines found.")
-print("-" * 20) # Added a separator for clarity
+ORCHESTRATE_AGENT_ID = os.environ.get('ORCHESTRATE_AGENT_ID')
+agent_engine = agent_engines.get(ORCHESTRATE_AGENT_ID)
 
 
-agent_engine = agent_engines.get('projects/789872749985/locations/us-central1/reasoningEngines/8928746901075918848')
+agent_engine = agent_engines.get(ORCHESTRATE_AGENT_ID)
 agent_engine.delete(force=True)
-#agent_engines.delete('projects/789872749985/locations/us-central1/reasoningEngines/8928746901075918848')
-
-
