d2094c4f79d9f58bb6ddc5e2a76e93e6ee5f3cfd
Fabrice <neo.oseis@gmail.com>
2025-07-07 03:08:09 -0400

    Update remote_delete.py

--- runners/remote_delete.py
+++ runners/remote_delete.py
@@ -5,6 +5,7 @@ import os
 ORCHESTRATE_AGENT_ID = os.environ.get('ORCHESTRATE_AGENT_ID')
 agent_engine = agent_engines.get(ORCHESTRATE_AGENT_ID)
 
+
 agent_engine = agent_engines.get(ORCHESTRATE_AGENT_ID)
 agent_engine.delete(force=True)
 
